301532aad571e1b00fbab6d2a573dd158394c8fe
DhruvGupta <143245356+0DhruvHere0@users.noreply.github.com>
2026-07-15 19:26:58 +0530

    Time: 1 ms (21.15%), Space: 8.5 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        vector<int> listOdd;
-        vector<int> listEven;
-        for (int i=1; i<=2*n; i++){
-            if (i%2==1){
-                listOdd.emplace_back(i);
-            }
-            else if (i%2==0){
-                listEven.emplace_back(i);
-            }
-        }
-        int sumOdd= accumulate(begin(listOdd),end(listOdd),0);
-        int sumEven= accumulate(begin(listEven), end(listEven),0);
+        int sumEven= (n)*(n+1);
+        int sumOdd= pow(n,2);
         return gcd(sumOdd, sumEven);
     }
 };
