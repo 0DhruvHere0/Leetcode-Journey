6bb0338c6238364ab330570cc8df3529789dfea9
DhruvGupta <143245356+0DhruvHere0@users.noreply.github.com>
2026-07-15 15:30:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.49%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        a= list(map(int, str(x)))
+        if (x%sum(a)==0):
+            return sum(a)
+        return -1
