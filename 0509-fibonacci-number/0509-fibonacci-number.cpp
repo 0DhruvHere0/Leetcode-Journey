124b800b9e30434ac8ec7f85822d634a2044c633
DhruvGupta <143245356+0DhruvHere0@users.noreply.github.com>
2026-07-24 11:44:56 +0530

    Time: 13 ms (31.8%), Space: 7.7 MB (65.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,22 +1,11 @@
 class Solution {
+private:
+    int fibi(int a){
+        if (a<=1) return a;
+        return fibi(a-1)+fibi(a-2);
+    }
 public:
     int fib(int n) {
-        vector<int> v(n+1);
-        if (n<2){
-            if (n==0){
-                return 0;
-            }
-            if (n==1){
-                return 1;
-            }
-        }
-        if (n>=2){
-            v[0]= 0;
-            v[1]=1;
-            for (int i=2; i<=n; i++){
-                v[i]= v[i-1]+v[i-2];
-            }
-        }
-        return v[n];
+        return fibi(n);
     }
 };
