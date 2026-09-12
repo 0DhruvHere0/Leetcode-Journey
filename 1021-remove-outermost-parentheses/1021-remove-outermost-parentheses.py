69a24bb3ac7245313119c5e64ac11e8843496074
DhruvGupta <143245356+0DhruvHere0@users.noreply.github.com>
2026-09-12 11:46:57 +0530

    Time: 4 ms (22.53%), Space: 19.4 MB (8.45%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        balance=0
+        result=""
+        for i in range(len(s)):
+            if s[i]=='(':
+                if balance>0:
+                    result+=s[i]
+                balance+=1
+            else:
+                balance-=1
+                if balance>0:
+                    result+=s[i]
+        return result
