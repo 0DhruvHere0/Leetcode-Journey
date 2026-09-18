28f01a87200b35e002eae6cb5794ea9fc0b6cc60
DhruvGupta <143245356+0DhruvHere0@users.noreply.github.com>
2026-09-18 12:46:40 +0530

    Time: 0 ms (100%), Space: 12.3 MB (99.96%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int a;
-        int b;
-        int c;
-        a= nums.size();
-        if (a==0){
-            return {};
-        }
-        if (a==1){
+        int n= nums.size();
+        if (n==1){
             return 0;
         }
-        if (a>1){
-            b= *max_element(nums.begin(), nums.end());
-            for (int i= 0; i<a; i++){
-                if (nums[i]==b){
-                    c= i;
-                }
+        if (nums[0]>nums[1]){
+            return 0;
+        }
+        if (nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+        int low=1;
+        int high= n-2;
+        while (low<=high){
+            int mid= low+(high-low);
+            if (nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+                return mid;
+            }
+            if (nums[mid]<nums[mid+1]){
+                low= mid+1;
+            }
+            else{
+                high= mid-1;
             }
         }
-        return c;
+        return low;
     }
 };
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
