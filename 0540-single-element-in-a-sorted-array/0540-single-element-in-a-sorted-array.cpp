91b3f599ac49c8897c2f44830b2074e8e1a4ce1b
DhruvGupta <143245356+0DhruvHere0@users.noreply.github.com>
2026-09-18 10:53:19 +0530

    Time: 0 ms (100%), Space: 25.1 MB (99.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        for (int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        for (const auto& val:mpp){
-            if (val.second==1){
-                return val.first;
-            }
+        int n= nums.size();
+        if (n==0) return nums[0];
+        else if (nums[0]!=nums[1]) return nums[0];
+        else if (nums[n-1]!=nums[n-2]) return nums[n-1];
+        int low=1;
+        int high= n-2;
+        while (low<=high){
+            int mid= low+(high-low)/2;
+            if (nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+            else if ((mid%2!=0 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])) low= mid+1;
+            else high= mid-1;
         }
         return -1;
     }
 };
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
