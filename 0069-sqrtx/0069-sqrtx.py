bdd2f211f74740ffab866916add6964f8ac80fc9
DhruvGupta <143245356+0DhruvHere0@users.noreply.github.com>
2026-09-22 10:48:17 +0530

    Time: 1 ms (81.92%), Space: 19.5 MB (5.25%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**0.5)
+        if x==0:
+            return 0
+        n=x
+        while True:
+            new_n= 0.5*(n+(x/n))
+            if new_n>=n:
+                return int(n)
+            n= new_n
+            
